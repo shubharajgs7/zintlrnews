a87ddba66b45ea8eb15b30256da9a942b79cb1b5
Shubharaj <shubharajgs7@gmail.com>
2024-01-05 00:46:21 +0530

    changes

--- src/components/TopNews/TopStories.js
+++ src/components/TopNews/TopStories.js
@@ -1,13 +1,11 @@
-import React, { useRef, useState } from 'react';
+import React from "react";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-
+import { Swiper, SwiperSlide } from "swiper/react";
 
 export default function TopStories() {
   return (
-    <div style={{marginTop:"50px"}}>
-        <h2>Top Stories for you</h2>
+    <div style={{ marginTop: "50px" }}>
+      <h2>Top Stories for you</h2>
       <Swiper
         slidesPerView={4}
         centeredSlides={true}
@@ -17,15 +15,15 @@ export default function TopStories() {
           clickable: true,
         }}
       >
-        <SwiperSlide >Slide 1</SwiperSlide>
-        <SwiperSlide >Slide 2</SwiperSlide>
-        <SwiperSlide >Slide 3</SwiperSlide>
-        <SwiperSlide >Slide 4</SwiperSlide>
-        <SwiperSlide >Slide 5</SwiperSlide>
-        <SwiperSlide >Slide 6</SwiperSlide>
-        <SwiperSlide >Slide 7</SwiperSlide>
-        <SwiperSlide >Slide 8</SwiperSlide>
-        <SwiperSlide >Slide 9</SwiperSlide>
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
       </Swiper>
     </div>
   );
